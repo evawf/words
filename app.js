e438f75b21b0c0773e8868e1e465dffe56d3ada2
eva.wang <eva.wang81@gmail.com>
2024-06-07 16:59:56 +0800

    updated app.js

--- app.js
+++ app.js
@@ -51,11 +51,11 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
+      maxAge: sessionLifetime,
       sameSite: "lax",
       httpOnly: false,
       secure: false,
-      domain: "localhost",
+      // domain: "localhost",
     },
   })
 );
