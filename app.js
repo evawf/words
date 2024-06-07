9d1274d1a9056df614ac2b45b872d84854399cfe
eva.wang <eva.wang81@gmail.com>
2024-06-07 17:19:17 +0800

    update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
